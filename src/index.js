cc6c6bfdd75c3e9271205d7c1bad2220d7d56891
Pankaj086 <pankajkumar086420@gmail.com>
2025-02-01 13:26:41 +0530

    Added Utility functions (Api [Error,Response])

--- src/index.js
+++ src/index.js
@@ -6,4 +6,16 @@ dotenv.config({
     path:'./env'
 })
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.on("error",(error)=>{
+        console.log("Error Not Able to Connect: ",error);
+        throw error;
+    })
+    app.listen(process.env.PORT || 3000, ()=>{
+        console.log(`Server Started at Port ${process.env.PORT}`);
+    });
+}) 
+.catch((err)=>{
+    console.log("MongoDB Connection Failed: ",err);
+})
