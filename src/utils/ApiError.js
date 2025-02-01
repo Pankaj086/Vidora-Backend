e8ed18f3f1ddb1e7442e1d9458f334564fe6f477
Pankaj086 <pankajkumar086420@gmail.com>
2025-02-01 14:18:30 +0530

    Modified ApiError.js

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,21 +1,34 @@
 class ApiError extends Error {
     constructor(
-        statusCode,
-        message = "Something went wrong",
-        errors = [],
-        stack = ""
+        statusCode,             
+        // HTTP status code for the error (e.g., 404, 500)
+        message = "Something went wrong",  
+        // Default message if no message is provided
+        errors = [],            
+        // An optional array to hold more specific errors or details
+        stack = ""             
+        // Optional custom stack trace
     ){
-        super(message)
-        this.statusCode = statusCode
-        this.data = null
-        this.message = message
-        this.success = false
-        this.errors = errors
+        super(message)           
+        // Calls the parent class (Error) constructor with the message
+        this.statusCode = statusCode  
+        // Sets the HTTP status code (e.g., 400, 500)
+        this.data = null              
+        // Placeholder for additional data that may be associated with the error
+        this.message = message        
+        // Sets the message property
+        this.success = false          
+        // A flag indicating failure (as opposed to success)
+        this.errors = errors          
+        // Stores any additional error details in an array
 
+        // If a custom stack trace is provided, it will be used, otherwise, a default stack trace is captured
         if(stack){
-            this.stack = stack
+            this.stack = stack      
+            // Use the custom stack trace
         } else{
-            Error.captureStackTrace(this,this.constructor);
+            Error.captureStackTrace(this, this.constructor);  
+            // Capture the stack trace for the error
         }
     }
 }
